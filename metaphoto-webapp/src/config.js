9d992422223e1638e19ca0ba0cd8f044305bd6eb
Al Miller <al.miller@ajmiller.net>
2019-07-25 12:28:40 -0700

    parameterize config values

--- metaphoto-webapp/src/config.js
+++ metaphoto-webapp/src/config.js
@@ -1,15 +1,15 @@
 export default {
     s3: {
-        REGION: "us-west-2",
-        BUCKET: "metaphoto-us-west-2-dev-metaphotodatabucket-8jec596nfltl"
+        REGION: process.env.REACT_APP_REGION,
+        DATA_BUCKET: process.env.REACT_APP_DATA_BUCKET
     },
     apiGateway: {
-        REGION: "us-west-2",
+        REGION: process.env.REACT_APP_REGION,
         NAME: "metaphoto-api",
-        URL: "https://api.metaphoto.ajmiller.net/dev"
+        URL: process.env.REACT_APP_API_URL
     },
     cognito: {
-        REGION: "us-west-2",
+        REGION: process.env.REACT_APP_REGION,
         USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
         APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
         IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID
