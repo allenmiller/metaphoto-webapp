b6460d3effa9d4fd01f1aa47d62bf4b74a33be83
Al Miller <al.miller@ajmiller.net>
2019-07-26 13:34:16 -0700

    forgot to add authentication reducer to root

--- metaphoto-webapp/src/reducers/index.js
+++ metaphoto-webapp/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+
+import authentication from './authentication';
 import time from './time';
 
 export default combineReducers({
+    authentication,
     time
 })
