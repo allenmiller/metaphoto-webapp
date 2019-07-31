031b7e5ec0c861de5fcf00110555d79f65dba6bf
Al Miller <al.miller@ajmiller.net>
2019-07-30 21:50:53 -0700

    first try at rendering film stock table

--- metaphoto-webapp/src/containers/FilmStocks.js
+++ metaphoto-webapp/src/containers/FilmStocks.js
@@ -6,7 +6,8 @@ import { API } from "aws-amplify";
 import {bindActionCreators} from "redux";
 import {withRouter} from "react-router-dom";
 import {connect} from "react-redux";
-
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
 import {addFilmStock, setIsAuthenticated} from "../actions/actions";
 
@@ -24,13 +25,32 @@ class FilmStocks extends Component {
         API.get(apiName, getFilmStocks, {})
             .then(response => {
                 console.log("in callback", response);
-                this.props.addFilmStock(response.data)
+                this.props.addFilmStock(response)
+            })
+            .catch(error => {
+                console.log("error in getFilmStocks()", error);
             })
     }
+
     render() {
+        const columns = [
+            {
+                Header: "Name",
+                accessor: "primaryHashKey"
+            },
+            {
+                Header: "Format",
+                accessor: d => d.data.filmFormat
+            }
+
+        ];
         return (
             <div className="FilmStocks">
             Here are some film stocks.
+                <ReactTable
+                    data = {this.props.filmStocks}
+                    columns = {columns}
+                />
             </div>
         );
     }
